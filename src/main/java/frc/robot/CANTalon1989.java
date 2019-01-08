83e16160d19614ab2013b5cbd7410821cd606b05
NoobishNoob <mintymatt1234@gmail.com>
2019-01-08 17:34:27 -0500

    First commit

---
+++ src/main/java/frc/robot/CANTalon1989.java
@@ -0,0 +1,67 @@
+package org.usfirst.frc.team1989.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Timer;
+
+public class CANTalon1989 extends WPI_TalonSRX{
+	public double maxI = 0.0; 
+	public double lasttimer;
+	public Timer t1= new Timer();
+	public double factor = 1.0;
+	private double dSpeed = 0;
+	public int overcurrent = 0;
+	public double lastcurrent = 0;
+	
+	/*public CANTalon1989(int deviceNumber, int controlPeriodMs, int enablePeriodMs) {
+		super(deviceNumber, controlPeriodMs, enablePeriodMs);
+		// TODO Auto-generated constructor stub
+		t1.start();
+		this.overcurrent = 0;
+		lasttimer = t1.get();
+	}
+	public CANTalon1989(int deviceNumber, int controlPeriodMs) {
+		super(deviceNumber, controlPeriodMs);
+		// TODO Auto-generated constructor stub
+		this.overcurrent = 0;
+		t1.start();
+		lasttimer = t1.get();
+	}*/
+
+	public CANTalon1989(int deviceNumber) {
+		super(deviceNumber);
+		// TODO Auto-generated constructor stub
+		t1.start();
+		this.overcurrent = 0;
+		lasttimer = t1.get();
+	}
+
+	public void set(double speed)
+	{
+		this.dSpeed = speed;
+		
+		if (this.maxI > 0 && t1.get() > lasttimer +.05)
+		{
+			double curr = this.getOutputCurrent();
+			this.lastcurrent = curr;
+			if (curr > this.maxI || factor<1) 
+			{
+				this.overcurrent++;
+				factor = factor *maxI/curr;
+				}
+			if( factor > 1 )
+			{
+				factor = 1 ;
+			}
+			dSpeed = speed * factor;
+			t1.reset();
+			lasttimer = t1.get();
+		}
+		super.set(dSpeed);
+	}
+	public String getSmartDashboardType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,40 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+  // For example to map the left and right motors, you could define the
+  // following variables to use with your drivetrain subsystem.
+  // public static int leftMotor = 1;
+  // public static int rightMotor = 2;
+
+  // If you are using multiple modules, make sure to define both the port
+  // number and the module. For example you with a rangefinder:
+  // public static int rangefinderPort = 1;
+  // public static int rangefinderModule = 1;
+
+  static CANTalon1989 frontLeft = new CANTalon1989(6);
+	static CANTalon1989 backLeft = new CANTalon1989(7);
+	static CANTalon1989 frontRight = new CANTalon1989(3);
+	static CANTalon1989 backRight = new CANTalon1989(9);
+	static CANTalon1989 armsLeft = new CANTalon1989(2);
+	static CANTalon1989 armsRight = new CANTalon1989(1);
+	static CANTalon1989 towerLeft = new CANTalon1989(8);
+	static CANTalon1989 towerRight = new CANTalon1989(5);
+	
+	static Servo servo = new Servo(0);
+	public static ADXRS450_Gyro gyro = new ADXRS450_Gyro();  
+
+
+}
